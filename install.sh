6a44838ab52c8cbb240aa042b321493ad5240d8b
avm <anoop.vm@gmail.com>
2018-08-29 15:43:14 +0000

    Almost final

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Labeling the node for prometheus/grafana..."
-kubectl label node &1 monitoring=telemetry --overwrite
+kubectl label node $1 monitoring=telemetry --overwrite
 echo "Creating PV..."
 kubectl create -f telemetry_pv.yaml
 echo "Creating PVs..."
@@ -8,3 +8,4 @@ echo "Creating PODs..."
 #kubectl create -f node-exporter.yaml
 kubectl create -f prometheus-rc.yaml
 kubectl create -f grafana-rc.yaml
+kubectl create -f alertmanager.yaml
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,5 @@
 echo "Unlabeling the node for prometheus/grafana..."
-kubectl label node &1 monitoring-
-echo "Deleting mount folder..."
-sudo rm -rf /var/lib/docker/containers/telemetry
+kubectl label node $1 monitoring-
 echo "Deleting PV..."
 kubectl delete -f telemetry_pv.yaml
 echo "Deleting PVs..."
@@ -10,4 +8,5 @@ echo "Deleting PODs..."
 kubectl delete -f node-exporter.yaml
 kubectl delete -f prometheus-rc.yaml
 kubectl delete -f grafana-rc.yaml
+kubectl delete -f alertmanager.yaml
 
