31e9bfaf75f12336dbb8b33b52ff65d2c67c4fb8
avm <anoop.vm@gmail.com>
2018-09-04 15:19:14 +0000

    Grafana changes

--- create_config.sh
+++ create_config.sh
@@ -10,5 +10,5 @@ sudo cp rules.yaml /var/lib/docker/containers/telemetry
 sudo cp grafana.ini /var/lib/docker/containers/telemetry
 sudo cp ldap.toml /var/lib/docker/containers/telemetry
 sudo cp prometheus-datasource.yaml /var/lib/docker/containers/telemetry/provisioning/datasources
-sudo cp node_exporter.json /var/lib/docker/containers/telemetry/provisioning/dashboards
+sudo cp node_metrics.json /var/lib/docker/containers/telemetry/provisioning/dashboards
 sudo cp k8s_cluster.json /var/lib/docker/containers/telemetry/provisioning/dashboards
