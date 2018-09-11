7ba234dc69de6e3887c17e6353aacd62a62ecdb5
anoopvm <anoop.vm@gmail.com>
2018-09-11 13:34:41 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 echo "Unlabeling the node for prometheus/grafana..."
 kubectl label node $1 monitoring-
 echo "Deleting PV..."
-kubectl delete -f telemetry_pv.yaml
-echo "Deleting PVs..."
 kubectl delete -f telemetry_pvc.yaml
+echo "Deleting PVs..."
+kubectl delete -f telemetry_pv.yaml
 echo "Deleting PODs..."
 kubectl delete -f prometheus-svc.yaml
 kubectl delete -f grafana-svc.yaml
